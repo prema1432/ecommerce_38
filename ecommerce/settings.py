7ca7c2d51b9ef53513b6ae8e74063e995689f2ae
Premanath Talamarla <premanath@syoft.com>
2024-03-04 19:45:14 +0530

    ecommerce static

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -115,6 +115,18 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+# local develoipment
+STATICFILES_DIRS = [
+    BASE_DIR / "static/",
+]
+
+# production devel
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
